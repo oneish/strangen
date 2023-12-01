f06df419836bc360f6afc3dcc73a7d79b7460c45
oneish <stuartdavis@oneish.com>
2023-12-01 17:26:34 +0000

    remove space_a and abstraction_a

--- sauce/strange/strange.h
+++ sauce/strange/strange.h
@@ -52,65 +52,6 @@ struct std::hash<strange::operation_a>
     }
 };
 
-namespace strange
-{
-inline auto operator==(strange::abstraction_a const & lhs, strange::abstraction_a const & rhs) -> bool
-{
-    return lhs.parameters() == rhs.parameters()
-        && lhs.name() == rhs.name()
-        && lhs.parents() == rhs.parents()
-        && lhs.operations() == rhs.operations()
-        && lhs.thing() == rhs.thing();
-}
-}
-
-template<>
-struct std::hash<strange::abstraction_a>
-{
-    inline auto operator()(strange::abstraction_a const & abs) const -> size_t
-    {
-        std::size_t h = 0;
-        for (auto const & param : abs.parameters())
-        {
-            h ^= std::hash<strange::parameter_a>{}(param);
-        }
-        h ^= std::hash<std::string>{}(abs.name());
-        for (auto const & parent : abs.parents())
-        {
-            h ^= std::hash<std::string>{}(parent);
-        }
-        for (auto const & oper : abs.operations())
-        {
-            h ^= std::hash<strange::operation_a>{}(oper);
-        }
-        h ^= std::hash<std::string>{}(abs.thing());
-        return h;
-    }
-};
-
-namespace strange
-{
-inline auto operator==(strange::space_a const & lhs, strange::space_a const & rhs) -> bool
-{
-    return lhs.name() == rhs.name()
-        && lhs.abstractions() == rhs.abstractions();
-}
-}
-
-template<>
-struct std::hash<strange::space_a>
-{
-    inline auto operator()(strange::space_a const & spc) const -> size_t
-    {
-        std::size_t h = std::hash<std::string>{}(spc.name());
-        for (auto const & abs : spc.abstractions())
-        {
-            h ^= std::hash<strange::abstraction_a>{}(abs);
-        }
-        return h;
-    }
-};
-
 #include "definition/strange__definition__parameter.h"
 
 namespace strange
