74e7535a28cf0e7c956e9a34d80945d7d5444222
oneish <stuartdavis@oneish.com>
2023-12-12 18:00:14 +0000

    added deduction guide for strange vector

--- sauce/strange/strange.h
+++ sauce/strange/strange.h
@@ -1,2 +1,10 @@
 #pragma once
 #include "space/strange__space.h"
+
+namespace strange
+{
+// deduction guide
+template<class InputIt>
+vector(InputIt, InputIt)
+    -> vector<typename std::iterator_traits<InputIt>::value_type>;
+}
